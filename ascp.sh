496d94a90b6990e72ca2a5dfc8c08a507125f410
guangchun <hanguangchun@gmail.com>
2015-07-24 11:32:45 +0800

    ERR file download support

--- ascp.sh
+++ ascp.sh
@@ -1,16 +1,17 @@
-SRR_Acc_file=$1
+XRR_Acc_file=$1 #XRR for SRR or ERR
 
-fasp_base_url=anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/SRR
+fasp_base_url=anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/
 aspera_base=/home/hangc/.aspera/connect
 
 while read line
 do
     srr_prefix=${line:0:6}
-    fasp_url=${fasp_base_url}/${srr_prefix}/${line}/${line}.sra
+    xrr=${line:0:3}
+    fasp_url=${fasp_base_url}/${xrr}/${srr_prefix}/${line}/${line}.sra
     if [ -f ${line}.sra -a ! -f ${line}.sra.aspx ];then
 	echo "${line}.sra already exists, skip..."
     else
 	echo "downloading $line"
 	${aspera_base}/bin/ascp -i ${aspera_base}/etc/asperaweb_id_dsa.openssh -k3 -T -l20m ${fasp_url} . ##-T will disable encryption...
     fi
-done < ${SRR_Acc_file}
+done < ${XRR_Acc_file}
