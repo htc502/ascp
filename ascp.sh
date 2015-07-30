1e319276e3ab792a7e848efb17bd79fbf0f7e820
guangchun <hanguangchun@gmail.com>
2015-07-30 09:52:48 +0800

    tweak ascp(v3.5.4) options

--- ascp.sh
+++ ascp.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 XRR_Acc_file=$1 #XRR for SRR or ERR
 
 fasp_base_url=anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra
@@ -12,6 +13,7 @@ do
 	echo "${line}.sra already exists, skip..."
     else
 	echo "downloading $line"
-	${aspera_base}/bin/ascp -i ${aspera_base}/etc/asperaweb_id_dsa.openssh -k3 -T -l20m ${fasp_url} . ##-T will disable encryption...
+	#-T-> no encryption; -l&-m transfer rate; --policy overrides -Q and -U; -k->resume level
+	${aspera_base}/bin/ascp -i ${aspera_base}/etc/asperaweb_id_dsa.openssh -k3 -T -l20m -m1k --policy=fair ${fasp_url} .
     fi
 done < ${XRR_Acc_file}
